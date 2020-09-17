a5d39a0ec1fe6a025e64c774b75ee224d5a1b8a7
rfabisiak <fabisiakradoslaw@gmail.com>
2020-09-17 17:24:37 +0200

    Start here

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,19 +1,14 @@
 import Head from 'next/head';
-import { useEffect } from 'react';
-import Card from '../components/home/card/card';
 
 export default function Home(props) {
+  
   return (
     <div>
       <Head>
         <title>Create Next App</title>
         <link rel='icon' href='/favicon.ico' />
       </Head>
-      <div>
-        {props.data.map(photo=>(
-          <Card photo={photo}/>
-        ))}
-      </div>
+      <div>Duomly Next.js app starter</div>
     </div>
   );
 }
